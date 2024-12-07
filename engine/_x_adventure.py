dd8655e59ba861c02832a3a8acba1263a1973c31
HillKTON <56476391+HillKTON@users.noreply.github.com>
2024-12-07 17:30:30 +0500

    feat: добавлено управление цифрами

--- engine/_x_adventure.py
+++ engine/_x_adventure.py
@@ -18,24 +18,21 @@ class XAdventureGame:
     def __init__(self):
         self.LIMITS = (9, 9)  # y, x
 
-
     def printer(self, matrix: list[list[str]]) -> None:
         for row in matrix:
-            print('  '.join(row))
-
+            print("  ".join(row))
 
     def move(self, vector: str, memory: Memory) -> None:
         match vector:
-            case 'up':
+            case "up":
                 memory.head.y -= 1
-            case 'down':
+            case "down":
                 memory.head.y += 1
-            case 'left':
+            case "left":
                 memory.head.x -= 1
-            case 'right':
+            case "right":
                 memory.head.x += 1
         self.move_validate(memory)
-
 
     def move_validate(self, memory: Memory) -> None:
         if memory.head.y < 0:
@@ -48,24 +45,22 @@ class XAdventureGame:
         elif memory.head.x > self.LIMITS[1]:
             memory.head.x = 0
 
-
     def run(self):
         memory = Memory(head=Coordinates(0, 0))
         while True:
-            print(f'Head: {memory.head}')
-            matrix = [['-' for _ in range(10)] for _ in range(10)]
-            matrix[memory.head.y][memory.head.x] = 'x'
+            print(f"Head: {memory.head}")
+            matrix = [["-" for _ in range(10)] for _ in range(10)]
+            matrix[memory.head.y][memory.head.x] = "x"
             self.printer(matrix)
-            userinput = input('Direction: >> ')
-            match userinput:
-                case 'w':
-                    self.move('up', memory)
-                case 's':
-                    self.move('down', memory)
-                case 'a':
-                    self.move('left', memory)
-                case 'd':
-                    self.move('right', memory)
-                case 'exit':
-                    exit(0)
+            userinput = input("Direction: >> ")
+            if userinput in ("w", "2"):
+                self.move("up", memory)
+            elif userinput in ("s", "8"):
+                self.move("down", memory)
+            elif userinput in ("a", "a"):
+                self.move("left", memory)
+            elif userinput in ("d", "6"):
+                self.move("right", memory)
+            elif userinput in ('exit', 'q'):
+                break
             clear_screen()
