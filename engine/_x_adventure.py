06a397869bc2ece2e051a76fdeae1115eec39faa
HillKTON <56476391+HillKTON@users.noreply.github.com>
2024-12-07 19:45:33 +0500

    feat: перевод на русский; вывод дополнительной информации; вынос принтера в отдельный метод

--- engine/_x_adventure.py
+++ engine/_x_adventure.py
@@ -21,18 +21,18 @@ class Coordinates:
 @dataclass
 class Memory:
     head: Coordinates
-    point: Coordinates
+    target: Coordinates
     score: int
 
 
 class XAdventureGame:
     def __init__(self):
-        self.LIMITS = (9, 9)  # y, x
-        self.memory = Memory(head=Coordinates(0, 0), point=Coordinates(0, 0), score=0)
+        self.LIMITS = Coordinates(x=9, y=9)  # y, x
+        self.memory = Memory(head=Coordinates(0, 0), target=Coordinates(0, 0), score=0)
 
-    def printer(self, matrix: list[list[str]]) -> None:
-        for row in matrix:
-            print("  ".join(row))
+        self.SCREEN_SYMBOL = "."
+        self.PLAYER_SYMBOL = "X"
+        self.TARGET_SYMBOL = "0"
 
     def move(self, vector: str) -> None:
         if vector.lower() in Directions.up.value:
@@ -48,35 +48,58 @@ class XAdventureGame:
 
     def move_validate(self) -> None:
         if self.memory.head.y < 0:
-            self.memory.head.y = self.LIMITS[0]
-        elif self.memory.head.y > self.LIMITS[0]:
+            self.memory.head.y = self.LIMITS.y
+        elif self.memory.head.y > self.LIMITS.y:
             self.memory.head.y = 0
 
         if self.memory.head.x < 0:
-            self.memory.head.x = self.LIMITS[1]
-        elif self.memory.head.x > self.LIMITS[1]:
+            self.memory.head.x = self.LIMITS.x
+        elif self.memory.head.x > self.LIMITS.x:
             self.memory.head.x = 0
 
-    def point_generator(self, init: bool):
-        if self.memory.head == self.memory.point:
-            self.memory.point.x = randint(0, self.LIMITS[1])
-            self.memory.point.y = randint(0, self.LIMITS[0])
+    def target_generator(self, init: bool):
+        if self.memory.head == self.memory.target:
+            self.memory.target.x = randint(0, self.LIMITS.x)
+            self.memory.target.y = randint(0, self.LIMITS.x)
             if not init:
                 self.memory.score += 1
 
+    def screen_render(self):
+        matrix = [
+            [self.SCREEN_SYMBOL for _ in range(self.LIMITS.y + 1)]
+            for _ in range(self.LIMITS.x + 1)
+        ]
+        matrix[self.memory.head.y][self.memory.head.x] = self.PLAYER_SYMBOL
+        matrix[self.memory.target.y][self.memory.target.x] = self.TARGET_SYMBOL
+        for row in matrix:
+            print("  ".join(row))
+
+    def game_controls_view(self):
+        print(f"\nКоординаты игрока: {self.memory.head}\nСчёт: {self.memory.score}")
+
+        print(
+            "\nУправление:\n"
+            f"Вверх: {','.join(Directions.up.value)}\n"
+            f"Вниз: {','.join(Directions.down.value)}\n"
+            f"Лево: {','.join(Directions.left.value)}\n"
+            f"Право: {','.join(Directions.right.value)}"
+        )
+
+        print(
+            "\nИнформация:\n"
+            f"Обозначение игрока: {self.PLAYER_SYMBOL}\n"
+            f"Обозначение цели: {self.TARGET_SYMBOL}\n"
+        )
+
     def run(self):
-        self.point_generator(init=True)
+        self.target_generator(init=True)
         while True:
-            print(f"Head: {self.memory.head}")
-            print(f"Score: {self.memory.score}")
-            matrix = [["-" for _ in range(10)] for _ in range(10)]
-            matrix[self.memory.head.y][self.memory.head.x] = "x"
-            matrix[self.memory.point.y][self.memory.point.x] = "0"
-            self.printer(matrix)
-            userinput = input("Direction: >> ")
+            self.screen_render()
+            self.game_controls_view()
+            userinput = input("Направление: >> ")
             if userinput.lower() == "exit":
                 break
             else:
                 self.move(userinput)
-            self.point_generator(init=False)
+            self.target_generator(init=False)
             clear_screen()
