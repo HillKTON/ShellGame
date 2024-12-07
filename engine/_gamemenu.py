5a752befb08e315edf8125fd31e33e463518d22d
HillKTON <56476391+HillKTON@users.noreply.github.com>
2024-12-07 16:35:32 +0500

    refactor: game menu

--- engine/_gamemenu.py
+++ engine/_gamemenu.py
@@ -6,23 +6,23 @@ class GameMenu:
     def __init__(self):
         self.games = {"1": "Угадай число"}
 
-
     def main_menu(self):
         print("Доступный игры:")
         for index in self.games:
             print(f"{index}: {self.games[index]}")
         while True:
             user_input = input("Номер игры: >> ")
-            match user_input:
-                case "1":
-                    numbers = NumbersGame()
-                    numbers.run()
-                case "exit":
-                    break
-                case _:
-                    print("Неправильный индекс")
-
+            if user_input == "1":
+                NumbersGame().run()
+                break
+            elif user_input == "exit":
+                return "exit"
+            else:
+                print('Неверный индекс')
 
     def init(self):
-        clear_screen()
-        self.main_menu()
+        while True:
+            clear_screen()
+            status = self.main_menu()
+            if status == "exit":
+                break
